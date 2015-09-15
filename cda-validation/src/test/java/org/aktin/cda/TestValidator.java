fdfdbbf78fd45db32daf491f42aa88ffc74ef1b8
R.W.Majeed <raphael@majeed.de>
2015-09-15 14:31:26 +0200

    documents and tests for illegal CDA documents

--- cda-validation/src/test/java/org/aktin/cda/TestValidator.java
+++ cda-validation/src/test/java/org/aktin/cda/TestValidator.java
@@ -3,6 +3,7 @@ package org.aktin.cda;
 import java.io.IOException;
 import java.io.InputStream;
 
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.stream.StreamSource;
 import javax.xml.xpath.XPathExpressionException;
@@ -40,5 +41,33 @@ public class TestValidator {
 				Assert.assertFalse("Document validation should produce errors", res.isValid());
 			}
 		}
+	}
+	
+	@Test
+	public void validateErrorsForOtherDocuments() throws TransformerConfigurationException, IOException{
+		Validator v = new Validator();
+		InputStream in = getClass().getResourceAsStream("/CDAexample/invalid-syntax.xml");
+		Assert.assertTrue(in.available() > 0);
+		try {
+			v.validate(new StreamSource(in));
+			Assert.fail();
+		} catch (XPathExpressionException e) {
+			Assert.fail();
+		} catch (TransformerException e) {
+			// expected error
+		}
+		in.close();
+
+		// check other XML document
+		// should return result
+		in = getClass().getResourceAsStream("/CDAexample/other-document.xml");
+		try {
+			ValidationResult res = v.validate(new StreamSource(in));
+			// should not pass validation
+			Assert.assertFalse(res.isValid());
+		} catch (XPathExpressionException | TransformerException e) {
+			Assert.fail();
+		}
+		in.close();
 	}
 }
