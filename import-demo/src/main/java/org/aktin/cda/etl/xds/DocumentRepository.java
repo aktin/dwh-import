fbd7750280ea1ffd63648a7853bb0b5a7255c185
R.W.Majeed <raphael@majeed.de>
2015-09-16 13:10:50 +0200

    valid results for XDS.b interface with tests

--- import-demo/src/main/java/org/aktin/cda/etl/xds/DocumentRepository.java
+++ import-demo/src/main/java/org/aktin/cda/etl/xds/DocumentRepository.java
@@ -1,7 +1,6 @@
 package org.aktin.cda.etl.xds;
 
 import java.io.ByteArrayInputStream;
-import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import javax.jws.WebService;
@@ -20,16 +19,21 @@ import ihe.iti.xds_b._2007.RetrieveDocumentSetRequestType;
 import ihe.iti.xds_b._2007.RetrieveDocumentSetResponseType;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.ExtrinsicObjectType;
 import oasis.names.tc.ebxml_regrep.xsd.rim._3.IdentifiableType;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryError;
+import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryErrorList;
 import oasis.names.tc.ebxml_regrep.xsd.rs._3.RegistryResponseType;
 
 /**
  * Implementation of document repository.
  * @see <a href="http://wiki.ihe.net/index.php?title=XDS.b_Implementation#Example_Provide_and_Register_Document_Set-b_transaction">IHE Example</a>
+ * 
+ * <p>
+ * Example with annotated transactions: {@link http://wiki.ihe.net/index.php?title=Annotated_ProvideAndRegister.b_Transaction}
  * @author Raphael
  *
  */
 //this binds the SEI to the SIB
-@WebService(endpointInterface = "ihe.iti.xds_b._2007.DocumentRepositoryPortType")
+@WebService(endpointInterface = "ihe.iti.xds_b._2007.DocumentRepositoryPortType", targetNamespace="urn:ihe:iti:xds-b:2007", serviceName="DocumentRepository_Service")
 public class DocumentRepository implements DocumentRepositoryPortType {
 	private static final Logger log = Logger.getLogger(DocumentRepository.class.getName());
 	private ValidationService validator;
@@ -40,33 +44,75 @@ public class DocumentRepository implements DocumentRepositoryPortType {
 	@Override
 	public RegistryResponseType documentRepositoryProvideAndRegisterDocumentSetB(
 			ProvideAndRegisterDocumentSetRequestType body) {
+		if( body.getSubmitObjectsRequest() == null ){
+			log.warning("SubmitObjectRequest is null");
+		}else if( body.getSubmitObjectsRequest().getRegistryObjectList() == null ){
+			log.warning("SubmitObjectRequest.RegistryObjectList is null");
+		}
+		// The inner wrapper, RegistryObjectList, contains the actual metadata.
+		// All submissions must include a Submission Set object. 
+		
+		// Check mime type for stable document
 		for( JAXBElement<? extends IdentifiableType> ident : body.getSubmitObjectsRequest().getRegistryObjectList().getIdentifiable() ){
 			if( ident.getDeclaredType() == ExtrinsicObjectType.class ){
 				ExtrinsicObjectType e = (ExtrinsicObjectType)ident.getValue();
+				if( e.getObjectType().equals("urn:uuid:7edca82f-054d-47f2-a032-9b2a5b5186c1") ){
+					// stable document
+					if( !"text/xml".equals(e.getMimeType()) ){
+						// TODO error, mime type should be text/xml
+					}
+				}
 				log.info("ExtrinsicObject: id="+e.getId());
 				log.info("\tobjectType: "+e.getObjectType());
 				log.info("\tmimeType: "+e.getMimeType());
 				log.info("\tstatus: "+e.getStatus());
 			}
 		}
-		for( Document doc : body.getDocument() ){
-			log.info("Found document with id="+doc.getId()+" and length="+doc.getValue().length);
-			ValidationResult vr = null;
-			try {
-				vr = validator.validate(new StreamSource(new ByteArrayInputStream(doc.getValue())));
-			} catch (XPathExpressionException | TransformerException e) {
-				log.log(Level.WARNING,"Error during validation", e);
-				// TODO set error status
-			}
-			if( vr != null ){
-				// TODO report result
-			}
-			
+		if( body.getDocument().isEmpty() ){
+			return createErrorResponse(XDSConstants.ERR_DOC_MISSING, "Need exactly one document", null);
+		}else if( body.getDocument().size() > 1 ){
+			return createErrorResponse(XDSConstants.ERR_REPO_ERROR, "Please submit exactly one document at a time", null);
 		}
+		Document doc = body.getDocument().get(0);
+		log.info("Found document with id="+doc.getId()+" and length="+doc.getValue().length);
+		ValidationResult vr = null;
+		try {
+			vr = validator.validate(new StreamSource(new ByteArrayInputStream(doc.getValue())));
+		} catch (TransformerException e) {
+			return createErrorResponse(XDSConstants.ERR_DOC_INVALID_CONTENT, "Error while processing document", e);
+		} catch (XPathExpressionException e) {
+			return createErrorResponse(XDSConstants.ERR_REPO_ERROR, "Internal error", e);
+		}
+
 		RegistryResponseType resp = new RegistryResponseType();
-		// TODO need namespace or types from ebxml?
-		resp.setStatus("urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Success");
-		// TODO Errors via	resp.getRegistryErrorList()
+
+		if( vr.isValid() ){
+			resp.setStatus(XDSConstants.RESPONSE_SUCCESS);			
+		}else{
+			// failed
+			resp.setStatus(XDSConstants.RESPONSE_FAILURE);			
+
+			RegistryError re = new RegistryError();
+			re.setSeverity(XDSConstants.SEVERITY_ERROR);
+			re.setCodeContext("Validation failed"); // free error message
+			re.setErrorCode(XDSConstants.ERR_DOC_INVALID_CONTENT);
+
+			// errorCode is a text code taken from Table 4.1-11 Error Codes in Technical Framework volume 3
+			// add error list
+			RegistryErrorList rel = new RegistryErrorList();
+			rel.getRegistryError().add(re);
+			rel.setHighestSeverity(XDSConstants.SEVERITY_ERROR);
+
+			vr.forEachErrorMessage(msg -> {
+				RegistryError e = new RegistryError();
+				e.setSeverity(XDSConstants.SEVERITY_ERROR);
+				e.setCodeContext(msg); // free error message
+				e.setErrorCode(XDSConstants.ERR_DOC_INVALID_CONTENT);
+				rel.getRegistryError().add(e);
+			});
+			resp.setRegistryErrorList(rel);			
+		}
+
 		return resp;
 	}
 
@@ -74,10 +120,32 @@ public class DocumentRepository implements DocumentRepositoryPortType {
 	public RetrieveDocumentSetResponseType documentRepositoryRetrieveDocumentSet(RetrieveDocumentSetRequestType body) {
 		RetrieveDocumentSetResponseType resp = new RetrieveDocumentSetResponseType();
 		RegistryResponseType rr = new RegistryResponseType();
+		
+		// follow IHE
+		rr.setStatus(XDSConstants.RESPONSE_FAILURE);
+		RegistryError re = new RegistryError();
+		re.setSeverity(XDSConstants.SEVERITY_ERROR);
+		re.setErrorCode(XDSConstants.ERR_REPO_ERROR);
+		RegistryErrorList rel = new RegistryErrorList();
+		rel.getRegistryError().add(re);
+		
 		resp.setRegistryResponse(rr);
-		rr.setStatus("FAIL!"); // TODO follow IHE
-		//throw new UnsupportedOperationException();
 		return resp;
 	}
 
+	public static RegistryResponseType createErrorResponse(String code, String message, Throwable error){
+		RegistryResponseType resp = new RegistryResponseType();
+		// failure
+		resp.setStatus(XDSConstants.RESPONSE_FAILURE);
+		RegistryError re = new RegistryError();
+		re.setSeverity(XDSConstants.SEVERITY_ERROR);
+		re.setCodeContext(message); // free error message
+		
+		// errorCode is a text code taken from Table 4.1-11 Error Codes in Technical Framework volume 3
+		re.setErrorCode(code);
+		RegistryErrorList rel = new RegistryErrorList();
+		rel.getRegistryError().add(re);
+		resp.setRegistryErrorList(rel);
+		return resp;
+	}
 }
---
+++ import-demo/src/main/java/org/aktin/cda/etl/xds/XDSConstants.java
@@ -0,0 +1,122 @@
+package org.aktin.cda.etl.xds;
+
+/**
+ * Error codes from IHE_ITI_TF_Vol3.pdf, Table 4.2.4.1-2: Error Codes
+ * 
+ * @author Raphael
+ *
+ */
+public class XDSConstants {
+
+	public static final String RESPONSE_FAILURE = "urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Failure";
+	public static final String RESPONSE_SUCCESS = "urn:oasis:names:tc:ebxml-regrep:ResponseStatusType:Success";
+	//public static final String RESPONSE_PARTIAL_SUCCESS = "urn:ihe:iti:2007:ResponseStatusType:PartialSuccess";
+	
+	public static final String SEVERITY_ERROR = "urn:oasis:names:tc:ebxml-regrep:ErrorSeverityType:Error";
+	public static final String SEVERITY_WARNING = "urn:oasis:names:tc:ebxml-regrep:ErrorSeverityType:Warning";
+
+	/** An XDR Recipient queued the document for future manual matching to a patient */
+	public static final String ERR_DOC_QUEUED = "DocumentQueued";
+	
+	/** The recipient has rejected this submission because it detected that one of the documents does not match the metadata (e.g.,
+	 *  formatCode) or has failed other requirements for the document content.
+	 *  When the RegistryError element contains this error code , the @location shall contain the DocumentUniqueID of the document in error.
+	 *  If multiple documents are in error, there shall be a separate RegistryError element for each document in error 
+	 */
+	public static final String ERR_DOC_INVALID_CONTENT = "InvalidDocumentContent";
+	
+	/** DocumentEntry exists in metadata with no corresponding attached document */
+	public static final String ERR_DOC_MISSING = "XDSMissingDocument";
+
+	/** MIME package contains MIME part with Content-Id header not found in metadata */
+	public static final String ERR_DOC_META_MISSING = "XDSMissingDocumentMetadata";
+	
+	/** This warning is returned if extra metadata was present but not saved. */
+	public static final String ERR_EXTRA_META_NOT_SAVED = "XDSExtraMetadataNotSaved";
+	
+	// irrelevant error codes:
+	/*
+	 * PartialAppendContentNotProcessed
+	 * PartialFolderContentNotProcessed
+	 * PartialReplaceContentNotProcessed
+	 * PartialTransformNotProcessed
+	 * PartialTransformReplaceNotProcessed
+	 */
+	public static final String ERR_MISSING_DOCUMENT_METADATA = "XDSMissingDocumentMetadata";
+	
+	/** This error is thrown when the patient Id is required to match and does not. 
+	 * The codeContext shall indicate the value of the Patient Id and the nature of the conflict.
+	 */
+	public static final String ERR_PATID_DOESNOT_MATCH = "XDSPatientIdDoesNotMatch";
+	public static final String ERR_NON_IDENTICAL_HASH = "XDSNonIdenticalHash";
+
+	/**
+	 * Internal Error
+	 * The error codes XDSRegistryError or XDSRepositoryError shall be returned 
+	 * if and only if a more detailed code is not available from this table for the 
+	 * condition being reported.
+	 * If one of these error codes is returned, the attribute codeContext shall contain 
+	 * details of the error condition that may be implementation-specific.
+	 */
+	public static final String ERR_REG_ERROR = "XDSRegistryError";
+	
+	/** Too much activity */
+	public static final String ERR_REG_BUSY = "XDSRegistryBusy";
+	
+	/**
+	 * Repository was unable to access the Registry
+	 */
+	public static final String ERR_REG_NOT_AVAIL = "XDSRegistryNotAvailable";
+	
+	/**
+	 * Error detected in metadata. 
+	 * Actor name indicates where error was detected. 
+	 * (Document Recipient uses Repository error). 
+	 * codeContext indicates nature of problem.
+	 */
+	public static final String ERR_REG_METADATA_ERROR = "XDSRegistryMetadataError";
+	
+
+	/**
+	 * Internal Error
+	 * The error codes XDSRegistryError or XDSRepositoryError shall be returned 
+	 * if and only if a more detailed code is not available from this table for the 
+	 * condition being reported.
+	 * If one of these error codes is returned, the attribute codeContext shall contain 
+	 * details of the error condition that may be implementation-specific.
+	 */
+	public static final String ERR_REPO_ERROR = "XDSRepositoryError";
+
+	/** Too much activity */
+	public static final String ERR_REPO_BUSY = "XDSRepositoryBusy";
+	public static final String ERR_REPO_OUT_OF_RESOURCES = "XDSRepositoryOutOfResources";
+	
+	/**
+	 * Error detected in metadata. 
+	 * Actor name indicates where error was detected. 
+	 * (Document Recipient uses Repository error). 
+	 * codeContext indicates nature of problem.
+	 */
+	public static final String ERR_REPO_METADATA_ERROR = "XDSRepositoryMetadataError";
+	public static final String ERR_REPO_WRONG_UNIQUE_ID = "XDSRepositoryWrongRepositoryUniqueId";
+	
+	public static final String ERR_MISSING_REGISTRY_PACKAGE = "RegistryPackage missing";
+
+	/**
+	 * The document associated with the uniqueId is not available. 
+	 * This could be because the document is not available, the requestor is not authorized 
+	 * to access that document or the document is no longer available.
+	 */
+	public static final String ERR_DOCUMENT_UNIQUE_ID = "XDSDocumentUniqueIdError";
+	
+	// unique ids from 4.2.5.1 Submission Set Object UUIDs
+	
+	public static final String UUID_CLASS_SUBMISSIONSET = "urn:uuid:a54d6aa5-d40d-43f9-88c5-b4633d873bdd";
+	public static final String UUID_CLASS_CONTENTTYPECODE = "urn:uuid:aa543740-bdda-424e-8c96-df4873be8500";
+	public static final String UUID_PATIENTID_EXTERNAL_IDENT = "urn:uuid:6b5aea1a-874d-4603-a4bc-96a0a7b38446";
+	public static final String UUID_UNIQUEID_EXTERNAL_IDENT = "urn:uuid:96fdda7c-d067-4183-912e-bf5ee74998a8";
+	public static final String UUID_SOURCEID_EXTERNAL_IDENT = "urn:uuid:554ac39e-e3fe-47fe-b233-965d2a147832";
+	
+	//
+	public static final String UUID_DOCUMENT_STABLE = "urn:uuid:7edca82f-054d-47f2-a032-9b2a5b5186c1";
+}
