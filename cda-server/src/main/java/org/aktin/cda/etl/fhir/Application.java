147ea4dc7a240f93f7c5edbceaf0f5c9d144df3b
R.W.Majeed <raphael@majeed.de>
2016-09-12 22:54:16 +0200

    jaxrs application class needed by wildfly to load the endpoints

---
+++ cda-server/src/main/java/org/aktin/cda/etl/fhir/Application.java
@@ -0,0 +1,11 @@
+package org.aktin.cda.etl.fhir;
+
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/cda/fhir")
+public class Application extends javax.ws.rs.core.Application{
+	public Application(){
+//		URL url = getClass().getClassLoader().getResource("META-INF/MANIFEST.MF");
+		// doesn't work and will return wildfly manifest
+	}
+}
