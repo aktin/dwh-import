e56f3c652616e00e67f56557a28234350e13153b
R.W.Majeed <raphael@majeed.de>
2016-09-23 11:47:38 +0200

    more memory for demo server

--- demo-distribution/src/main/scripts/startup/java-server.bat
+++ demo-distribution/src/main/scripts/startup/java-server.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 REM ${name}
 SET mydir=%~dp0
-java -Djava.util.logging.config.file="%mydir%\logging.properties" -cp "%mydir%\lib\*" org.aktin.cda.etl.demo.Server %*
+java -Xmx1024m -Djava.util.logging.config.file="%mydir%\logging.properties" -cp "%mydir%\lib\*" org.aktin.cda.etl.demo.Server %*
