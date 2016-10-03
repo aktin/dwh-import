d2ad3318b64a63db209948f22df5a716d5e76e25
R.W.Majeed <raphael@majeed.de>
2016-10-04 00:52:52 +0200

    prepare release 0.6.2 with all submodules

--- demo-distribution/src/main/scripts/startup/curl-response.bat
+++ demo-distribution/src/main/scripts/startup/curl-response.bat
@@ -6,11 +6,11 @@ IF [%1] == [] GOTO usage
 IF [%2] == [] GOTO usage
 IF [%3] == [] GOTO localhost
 
-curl --silent -X POST "http://%1:%2/aktin/fhir/Binary" -H "Content-Type: text/xml" --data-binary "@%3"
+curl --silent -X POST "http://%1:%2/aktin/cda/fhir/Binary" -H "Content-Type: text/xml" --data-binary "@%3"
 GOTO end
 
 :localhost
-curl --silent -X POST "http://localhost:%1/aktin/fhir/Binary" -H "Content-Type: text/xml" --data-binary "@%2"
+curl --silent -X POST "http://localhost:%1/aktin/cda/fhir/Binary" -H "Content-Type: text/xml" --data-binary "@%2"
 GOTO end
 
 :usage
--- demo-distribution/src/main/scripts/startup/curl-statusonly.bat
+++ demo-distribution/src/main/scripts/startup/curl-statusonly.bat
@@ -6,11 +6,11 @@ IF [%1] == [] GOTO usage
 IF [%2] == [] GOTO usage
 IF [%3] == [] GOTO localhost
 
-curl --silent -w "%%{http_code}"  -X POST "http://%1:%2/aktin/fhir/Binary" -H "Content-Type: text/xml" --data-binary "@%3" -o NUL
+curl --silent -w "%%{http_code}"  -X POST "http://%1:%2/aktin/cda/fhir/Binary" -H "Content-Type: text/xml" --data-binary "@%3" -o NUL
 GOTO end
 
 :localhost
-curl --silent -w "%%{http_code}"  -X POST "http://localhost:%1/aktin/fhir/Binary" -H "Content-Type: text/xml" --data-binary "@%2" -o NUL
+curl --silent -w "%%{http_code}"  -X POST "http://localhost:%1/aktin/cda/fhir/Binary" -H "Content-Type: text/xml" --data-binary "@%2" -o NUL
 GOTO end
 
 :usage
