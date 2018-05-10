b9b870869b8b2d56c8fb84b4e616f66a037e8d1d
R.W.Majeed <raphael@majeed.de>
2018-05-10 22:45:56 +0200

    comment

--- cda-server/src/main/java/org/aktin/cda/CDAStatus.java
+++ cda-server/src/main/java/org/aktin/cda/CDAStatus.java
@@ -5,6 +5,7 @@ import java.util.Date;
 public class CDAStatus {
 	private CDASummary summary;
 	private Status status;
+	// XXX maybe add information/warning messages 
 	
 	public enum Status{
 		Created,
