06b0782e460f3939380af9bb06ebbe5250742c8f
Alexander Kombeiz <akombeiz@ukaachen.de>
2021-01-18 16:50:36 +0100

    added killProcess(), reworked execute() to use ProcessBuilder.redirect

--- script-executor/src/main/java/org/aktin/scripts/PythonRunner.java
+++ script-executor/src/main/java/org/aktin/scripts/PythonRunner.java
@@ -5,12 +5,13 @@ import org.aktin.dwh.admin.importer.enums.ImportState;
 import org.aktin.dwh.admin.importer.enums.PropertyKey;
 
 import javax.inject.Inject;
-import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.concurrent.TimeUnit;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -69,7 +70,7 @@ public class PythonRunner implements Runnable {
 
     public void cancel(String uuid) {
         if (runningId.equals(uuid)) {
-            process.destroy();
+            killProcess();
         } else {
             synchronized (queue) {
                 for (PythonScriptTask task : queue) {
@@ -86,73 +87,59 @@ public class PythonRunner implements Runnable {
     public void terminate() {
         exit = true;
         if (process != null) {
-            process.destroy();
+            killProcess();
         }
         synchronized (queue) {
             queue.notify();
         }
     }
 
-
-    //TODO get std output and write to file
-    //TODO Timeout
-    //TODO clean up output file if cancelled/interrupted
-    // Pythonprozess kann endloss sein
-    // Worker/Execution Warteschlange mit Timeout
-    private void execute(PythonScriptTask task) {
-        BufferedReader stdInput = null;
-        BufferedReader stdError = null;
+    private void killProcess() {
         try {
-            runningId = task.getId();
-            changeTaskState(task.getId(), ImportState.in_progress, Level.INFO);
+            process.destroy();
+            process.waitFor(10, TimeUnit.SECONDS);
+            process.destroyForcibly();
+            process.waitFor();
+        } catch (InterruptedException e) {
+            LOGGER.log(Level.WARNING, "Killing thread was interrupted", e);
+            Thread.currentThread().interrupt();
+        }
+    }
 
-            HashMap<String, String> map_properties = fileOperationManager.checkPropertiesFileForIntegrity(task.getId());
+
+    // write nur wenn abgeschlossen 
+    //TODO what about clean up ??? if cancelled/interrupted
+    private void execute(PythonScriptTask task) {
+        HashMap<String, String> map_properties = fileOperationManager.checkPropertiesFileForIntegrity(task.getId());
+        if (map_properties != null && !map_properties.isEmpty()) {
             String name_script = map_properties.get(PropertyKey.script.name());
             String path_script = fileOperationManager.getScriptPath(name_script);
-            String path_file = fileOperationManager.getFilePath(task.getId());
+            String path_folder = fileOperationManager.getFileFolder(task.getId());
+            String path_file = Paths.get(path_folder, map_properties.get(PropertyKey.filename.name())).toString();
 
-            process = new ProcessBuilder("python", path_script, task.getScriptMethod().name(), path_file).start();
-            stdInput = new BufferedReader(new InputStreamReader(process.getInputStream()));
-            stdError = new BufferedReader(new InputStreamReader(process.getErrorStream()));
+            long currentTime = System.currentTimeMillis();
+            File error = new File(new StringBuilder(path_folder).append(currentTime).append("_error").toString());
+            File output = new File(new StringBuilder(path_folder).append(currentTime).append("_output").toString());
+            try {
+                runningId = task.getId();
+                changeTaskState(task.getId(), ImportState.in_progress, Level.INFO);
 
-            String line;
-            while ((line = stdInput.readLine()) != null) {
-                System.out.println(line);
-                //TODO
-                // kein XML
-                // properties datei zeilenweise
-                // stdin zusammenfassungen / Ergebnis
-                // stderror warnung, error / alle Logs
-            }
-            while ((line = stdError.readLine()) != null) {
-                System.out.println(line);
-                //TODO
-                // kein XML
-                // properties datei zeilenweise
-                // stdin zusammenfassungen / Ergebnis
-                // stderror warnung, error / alle Logs
-            }
-            changeTaskState(task.getId(), ImportState.successful, Level.INFO);
-        } catch (IOException e) {
-            changeTaskState(task.getId(), ImportState.failed, Level.SEVERE);
-        } finally {
-            runningId = null;
-            process = null;
-            if (stdInput != null) {
-                try {
-                    stdInput.close();
-                } catch (IOException e) {
-                    LOGGER.log(Level.WARNING, "Closing of inputReader failed", e);
-                }
-            }
-            if (stdError != null) {
-                try {
-                    stdError.close();
-                } catch (IOException e) {
-                    LOGGER.log(Level.WARNING, "Closing of errorReader failed", e);
-                }
-            }
+                ProcessBuilder processBuilder = new ProcessBuilder("python", path_script, task.getScriptMethod().name(), path_file);
+                processBuilder.redirectError(ProcessBuilder.Redirect.appendTo(error));
+                processBuilder.redirectOutput(ProcessBuilder.Redirect.appendTo(output));
+                process = processBuilder.start();
 
+                if (!process.waitFor(4, TimeUnit.HOURS)) {
+                    changeTaskState(task.getId(), ImportState.timeout, Level.SEVERE);
+                    killProcess();
+                } else
+                    changeTaskState(task.getId(), ImportState.successful, Level.INFO);
+            } catch (IOException | InterruptedException e) {
+                changeTaskState(task.getId(), ImportState.failed, Level.SEVERE);
+            } finally {
+                runningId = null;
+                process = null;
+            }
         }
     }
 
