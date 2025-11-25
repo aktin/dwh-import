e0466a1052a0271acde36fecd3a017fc93b611bb
akomii <alexkombie@gmail.com>
2025-11-25 12:45:05 +0100

    cleaned up start.sh, set docker env to jammy

--- demo-distribution-docker/src/main/resources/start.sh
+++ demo-distribution-docker/src/main/resources/start.sh
@@ -3,23 +3,7 @@
 # Check if SERVER_PORT is set, otherwise default to 8080 (safety net)
 : "${SERVER_PORT:=8080}"
 
-CONFIG_FILE="/app/config/logging.properties"
-
 "$JAVA_HOME"/bin/java -Xmx1024m \
-  -Djava.util.logging.config.file="$CONFIG_FILE" \
   -cp "/app/lib/*" \
+  -Djava.util.logging.config.file="/app/config/logging.properties" \
   org.aktin.cda.etl.demo.Server "${SERVER_PORT}"
-
-
-      # java -Xmx1024m \
-      #  -Djava.util.logging.config.file=logging.properties \
-      #  -cp lib/\* \
-      #  org.aktin.cda.etl.demo.Server 8080
-      #
-      #
-      #
-      #    --add-modules java.activation,java.xml.bind,java.xml.ws \
-      #  --patch-module java.xml.ws.annotation="$mydir/lib/javax.annotation-api-1.2.jar" \
-      #  --add-opens java.base/java.io=ALL-UNNAMED \
-
-
